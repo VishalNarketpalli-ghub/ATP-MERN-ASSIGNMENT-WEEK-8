824bccd1cb36004ed072a982d83431fb85637291
Vishal Narketpalli <narketpallivishal@gmail.com>
2026-05-21 11:23:11 +0530

    footer changes

--- ATP-MERN-ASSIGNMENT-WEEK-8/Frontend/src/components/Footer.jsx
+++ ATP-MERN-ASSIGNMENT-WEEK-8/Frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer className="footer">
-            © {new Date().getFullYear()} User Management · Built with React
+            {new Date().getFullYear()} User Management · ATP Asignment - 8
         </footer>
     );
 }
